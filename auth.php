679191dac5c8a1420d05eb8257a0c2d6ab1058ee
Alan Vitório <alanvitorio1985@gmail.com>
2023-06-20 00:20:20 -0300

    adicionando dao

--- auth.php
+++ auth.php
@@ -12,6 +12,7 @@ require_once("templates/header.php");
             <label for="email">E-mail:</label>
             <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail">
           </div>
+
           <div class="form-group">
             <label for="password">Senha:</label>
             <input type="password" class="form-control" id="password" name="password" placeholder="Digite sua senha">
@@ -19,6 +20,7 @@ require_once("templates/header.php");
           <input type="submit" class="btn card-btn" value="Entrar">
         </form>
       </div>
+
       <div class="col-md-4" id="register-container">
         <h2>Criar Conta</h2>
         <form action="<?= $BASE_URL ?>auth_process.php" method="POST">
@@ -27,18 +29,22 @@ require_once("templates/header.php");
             <label for="email">E-mail:</label>
             <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail">
           </div>
+
           <div class="form-group">
             <label for="name">Nome:</label>
             <input type="text" class="form-control" id="name" name="name" placeholder="Digite seu nome">
           </div>
+
           <div class="form-group">
             <label for="lastname">Sobrenome:</label>
             <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Digite seu sobrenome">
           </div>
+
           <div class="form-group">
             <label for="password">Senha:</label>
             <input type="password" class="form-control" id="password" name="password" placeholder="Digite sua senha">
           </div>
+
           <div class="form-group">
             <label for="confirmpassword">Confirmação de senha:</label>
             <input type="password" class="form-control" id="confirmpassword" name="confirmpassword" placeholder="Confirme sua senha">
